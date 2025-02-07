ebd4c6948993494ece04f7240c041f04712d7214
Anamol Soman <anamolsoman2@gmail.com>
2025-02-07 10:13:43 +0530

    Env updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,10 +7,8 @@ module.exports = {
   solidity: "0.8.28",
   networks: {
     sepolia: {
-      url: "https://sepolia-eth.w3node.com/706502dfaca80e51178ed4edc37cf692b83b5cf0aedde7ec536776b3be95bee5/api",
-      accounts: [
-        "3bc6c6c39b1d73001c3155f221132e128e31545a4d48144fcab260bb02676cd1",
-      ],
+      url: `https://sepolia.infura.io/v3/${process.env.API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
