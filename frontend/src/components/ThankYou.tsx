a0c6e79b16cd75a0814c9c154779c26c51756c19
Anamol Soman <anamolsoman2@gmail.com>
2025-02-13 13:35:22 +0530

    Build issue fixed.

--- frontend/src/components/ThankYou.tsx
+++ frontend/src/components/ThankYou.tsx
@@ -1,5 +1,8 @@
-const ThankYou = () => {
-  return <div>Thank You for your coffee</div>;
-};
+import { useParams } from "react-router-dom";
+
+function ThankYou() {
+  const { id } = useParams(); // Access the 'id' param from the route
+  return <div>Thank you for your payment! ID: {id}</div>;
+}
 
 export default ThankYou;
