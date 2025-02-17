984086be7d869c8a2b8991252c07bb4abc902111
Anamol Soman <anamolsoman2@gmail.com>
2025-02-17 21:23:58 +0530

    Minor change.

--- frontend/src/components/ThankYou.tsx
+++ frontend/src/components/ThankYou.tsx
@@ -1,5 +1,5 @@
 const ThankYou = () => {
-  return <div className="text-white text-4xl">Thank You for your coffee</div>;
+  return <div className="text-white text-4xl">Thank You for your coffee!</div>;
 };
 
 export default ThankYou;
